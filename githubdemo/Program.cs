c756706da966965678b43e336f43fc9514d37254
NadineL1 <nadine.sorinder@gmail.com>
2025-09-18 14:15:41 +0200

    added string name

--- githubdemo/Program.cs
+++ githubdemo/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Nadine";
+            Console.WriteLine("Hello,"+ name);
         }
     }
 }
